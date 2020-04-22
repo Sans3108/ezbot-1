873cd5e87963c284ac50329bcf4be243ad142f11
futur <51336619+futurGH@users.noreply.github.com>
2020-04-22 13:22:25 -0400

    Indent attempt #3

--- commands/addClub.js
+++ commands/addClub.js
@@ -3,53 +3,53 @@ const { db, Discord, brawlStars, rethink } = require("../functions/requirePackag
 module.exports = {
 	name: 'addclub',
 	description: 'Adds a Club to the database.',
-  aliases: ['ac', 'newclub', 'pushclub', 'clubadd'],
+  	aliases: ['ac', 'newclub', 'pushclub', 'clubadd'],
 	usage: '[Club name] [Club tag] [Role ID]',
 	cooldown: 3,
 	guildOnly: true, 
 	args: true,
-  bannedGuilds: [],
-  allowedGuilds: [],
+  	bannedGuilds: [],
+  	allowedGuilds: [],
 	execute(message, args, bot, color) {
-		const perms = ["MANAGE_GUILD"];
-    const embed1 = new Discord.RichEmbed()
+	  	const perms = ["MANAGE_GUILD"];
+    	const embed1 = new Discord.RichEmbed()
 			.setColor(color.red)
-      .addField("ERROR: Insufficient permissions", "You don't have permission to run this command!");
+      		.addField("ERROR: Insufficient permissions", "You don't have permission to run this command!");
 		if (!message.member.hasPermission(perms)) return message.channel.send(embed1);
 
 		const embed2 = new Discord.RichEmbed()
 			.setColor(color.red)
 			.addField("ERROR: Invalid arguments provided", "You provided incorrect arguments!")
-      .addField("Proper usage:", `\`/addclub [Club name] [Club tag] [Role ID]\``)
+      		.addField("Proper usage:", `\`/addclub [Club name] [Club tag] [Role ID]\``)
 
 		if (!args[2] || args[2].startsWith("#") || !isNaN(args[0][0])) return message.channel.send(embed2);
 		const embed3 = new Discord.RichEmbed()
 			.setColor(color.green)
-      .setAuthor(message.member.displayName, message.author.displayAvatarURL)
-      .addField("**Success! Club added!**", `Added Club ${args[0]} to role ID ${args[2]}!`);
+      		.setAuthor(message.member.displayName, message.author.displayAvatarURL)
+      		.addField("**Success! Club added!**", `Added Club ${args[0]} to role ID ${args[2]}!`);
 
-    async function getClubInfo() {
-      let club = await brawlStars.getClub(args[1])
-      .catch(e => {
+    	async function getClubInfo() {
+      		let club = await brawlStars.getClub(args[1])
+      		.catch(e => {
 				throw new Error(e);
 			});
-      return club;
-    };
+      		return club;
+    	};
 		const titleCase = (name) => {
-      let lowerName = string.toLowerCase().split(" ");
-      for(let i = 0; i < lowerName.length; i++){
-        lowerName[i] = lowerName[i][0].toUpperCase() + lowerName[i].slice(1);
-      }
+      		let lowerName = string.toLowerCase().split(" ");
+      		for (let i = 0; i < lowerName.length; i++){
+        		lowerName[i] = lowerName[i][0].toUpperCase() + lowerName[i].slice(1);
+      		}
 			return lowerName.join(" ");
-   	};
+   		};
     
-    let clubName = titleCase(args[0]);
-    
-    rethink.db("clubs").table("list").insert({
-      name: clubName,
+    	let clubName = titleCase(args[0]);
+	    
+	  	rethink.db("clubs").table("list").insert({
+      		name: clubName,
 			tag: args[1],
 			id: args[2]
 		});
-		message.channel.send(embed3);
+	  	message.channel.send(embed3);
 	}
 };
