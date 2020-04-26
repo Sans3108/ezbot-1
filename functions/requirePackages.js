7b82cc6dd769e7c356cca8a3aed3285be48ae5bc
futur <51336619+futurGH@users.noreply.github.com>
2020-04-25 22:25:08 -0400

    fixing rethink

--- functions/requirePackages.js
+++ functions/requirePackages.js
@@ -1,5 +1,3 @@
-const r = require("rethinkdb");
-
 module.exports = {
   // no other solution came to mind to be able to use functions in every
   // file without re-declaring them in each file
@@ -12,12 +10,5 @@ module.exports = {
   get brawlStars() {
     return new this.brawlStarsJS.Client(process.env.BRAWL_TOKEN)
   },
-  rethink: r
+  rethink: require("rethinkdb")
 }
-
-let connection = null;
-
-r.connect( {host: process.env.RETHINK_HOST_IP }, function(err, conn) {
-    if (err) throw err;
-    module.exports.connection = conn;
-})
